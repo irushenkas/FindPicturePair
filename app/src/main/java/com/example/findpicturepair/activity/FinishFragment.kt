8b82029ae057235bf63932921713280be8907963
irushenka <sindirina@mail.ru>
2023-12-21 23:43:14 +0300

    finish page

--- app/src/main/java/com/example/findpicturepair/activity/FinishFragment.kt
+++ app/src/main/java/com/example/findpicturepair/activity/FinishFragment.kt
@@ -5,31 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.findpicturepair.R
+import com.example.findpicturepair.databinding.FragmentFinishBinding
 import com.example.findpicturepair.databinding.FragmentStartBinding
 
-class StartFragment : Fragment() {
+class FinishFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentStartBinding.inflate(
+        val binding = FragmentFinishBinding.inflate(
             inflater,
             container,
             false
         )
 
 
-        binding.start.setOnClickListener{
+        binding.home.setOnClickListener{
             findNavController()
                 .navigate(
-                    R.id.action_startFragment_to_finishFragment
+                    R.id.action_finishFragment_to_startFragment
                 )
         }
+
         return binding.root
     }
 }
